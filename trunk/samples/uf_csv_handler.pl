6aa67eb9db7bf8ab3150c1a50d9d84278bc93801
vrybdpkt <vrybdpkt@42d883e9-bd39-0410-b77b-8f19f4552143>
2007-11-18 21:08:36 +0000

    starting to add handlers

---
+++ trunk/samples/uf_csv_handler.pl
@@ -0,0 +1,63 @@
+#!/usr/bin/perl -I..
+
+use SnortUnified(qw(:ALL));
+
+$file = shift;
+$debug = 0;
+$UF_Data = {};
+$record = {};
+
+# The available handlers for SnortUnified.pm are
+# ("unified_opened", $UF);
+# ("unified2_event", $UF_Record);
+# ("unified2_packet", $UF_Record);
+# ("unified2_unhandled", $UF_Record);
+# ("unified2_record", $UF_Record); 
+# ("unified_record", $UF_Record);
+# ("read_data", ($readsize, $buffer));
+# ("read_header", $h);
+
+register_handler('unified2_record', \&printrec);
+
+$UF_Data = openSnortUnified($file);
+die unless $UF_Data;
+
+print("row");
+foreach $field ( @{$record->{'FIELDS'}} ) {
+    if ( $field ne 'pkt' ) { 
+        print("," . $field);
+    }
+}
+
+print("\n");
+
+while (read_records()){};
+
+closeSnortUnified();
+return 0;
+
+#############################################
+
+sub read_records() {
+  while ( $record = readSnortUnifiedRecord() ) {
+    
+    print($i++ . ",");
+  }
+
+  print("Exited while. Deadreads is $UF->{'DEADREADS'}.\n") if $debug;
+  
+  return 0;
+}
+
+sub printrec() {
+  $rec = shift;
+  print $UNIFIED2_TYPES->{$rec->{'TYPE'}};
+  foreach $field ( @{$rec->{'FIELDS'}} ) {
+    if ( $field ne 'pkt' ) {
+      print("," . $rec->{$field});
+    }
+  }
+  print("\n");
+
+}
+
