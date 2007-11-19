1a46baffe29d592625b0e723dcab282f6ac0b423
vrybdpkt <vrybdpkt@42d883e9-bd39-0410-b77b-8f19f4552143>
2007-11-19 02:26:03 +0000

    per sid handlers called "qualifiers"

--- trunk/samples/uf_csv_handler.pl
+++ trunk/samples/uf_csv_handler.pl
@@ -8,6 +8,7 @@ $debug = 0;
 $UF_Data = {};
 $record = {};
 
+# handlers will be run and regardless of the result processing will continue
 # The available handlers for SnortUnified.pm are
 # ("unified_opened", $UF);
 # ("unified2_event", $UF_Record);
@@ -19,31 +20,34 @@ $record = {};
 # ("read_header", $h);
 
 register_handler('unified2_record', \&printrec);
+register_handler('unified_record', \&printrec);
+show_handlers();
+
+# Qualifiers will be run, if any return a value < 1 
+# then the record will be discarded and processing will continue
+# with the next record in the file
+# Only one option for unified types
+register_qualifier(0,1,402, \&make_noise);
+register_qualifier(0,1,402, \&make_noise_fail);
+register_qualifier(0,1,402, \&make_noise_never);
+
+# But you can be granular with unified2 types
+register_qualifier($UNIFIED2_IDS_EVENT,1,402, \&make_noise);
+register_qualifier($UNIFIED2_PACKET,1,402, \&make_noise);
+show_qualifiers();
 
 $UF_Data = openSnortUnified($file);
 die unless $UF_Data;
 
-print("row");
-foreach $field ( @{$record->{'FIELDS'}} ) {
-    if ( $field ne 'pkt' ) { 
-        print("," . $field);
-    }
-}
-
-print("\n");
-
 while (read_records()){};
 
 closeSnortUnified();
-return 0;
+exit 0;
 
 #############################################
 
 sub read_records() {
-  while ( $record = readSnortUnifiedRecord() ) {
-    
-    print($i++ . ",");
-  }
+  while ( $record = readSnortUnifiedRecord() ) {}
 
   print("Exited while. Deadreads is $UF->{'DEADREADS'}.\n") if $debug;
   
@@ -52,13 +56,68 @@ sub read_records() {
 
 sub printrec() {
   $rec = shift;
-  print $UNIFIED2_TYPES->{$rec->{'TYPE'}};
+
+  # print $UNIFIED2_TYPES->{$rec->{'TYPE'}};
   foreach $field ( @{$rec->{'FIELDS'}} ) {
     if ( $field ne 'pkt' ) {
-      print("," . $rec->{$field});
+      print($rec->{$field} . ",");
     }
   }
-  print("\n");
+  print($i++ . "\n");
 
+}
+
+sub make_noise() {
+  $rec = shift;
+
+  print("#" x 70 . "\n");
+  print("#" x 20 . "\n");
+  print("#" x 20 . "\n");
+  print("#" x 20 . "\n");
+  print("#" x 20 . "\n");
+  print("#" x 20 . "\n");
+  print("#" x 20 . "\n");
+  print("#" x 20 . "\n");
+  print("#" x 20 . "\n");
+  print("#" x 20 . "\n");
+  print("#" x 70 . "\n");
+
+  return 1;
+}
+
+sub make_noise_fail() {
+  $rec = shift;
+
+  print("#" x 20 . "\n");
+  print("#" x 70 . "\n");
+  print("#" x 70 . "\n");
+  print("#" x 70 . "\n");
+  print("#" x 70 . "\n");
+  print("#" x 70 . "\n");
+  print("#" x 70 . "\n");
+  print("#" x 70 . "\n");
+  print("#" x 70 . "\n");
+  print("#" x 70 . "\n");
+  print("#" x 20 . "\n");
+
+  return 0;
+}
+
+sub make_noise_never() {
+  $rec = shift;
+
+  print("#" x 20 . "\n");
+  print("#" x 70 . "\n");
+  print("#" x 20 . "\n");
+  print("#" x 70 . "\n");
+  print("#" x 20 . "\n");
+  print("#" x 70 . "\n");
+  print("#" x 20 . "\n");
+  print("#" x 70 . "\n");
+  print("#" x 20 . "\n");
+  print("#" x 70 . "\n");
+  print("#" x 20 . "\n");
+
+  return 1;
 }
 
