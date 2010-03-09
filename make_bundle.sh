512a629f5264dbbde265607fd6cfb50f3f87b0bf
vrybdpkt <vrybdpkt@42d883e9-bd39-0410-b77b-8f19f4552143>
2010-03-09 00:11:34 +0000

    making a tarball per request.

--- make_bundle.sh
+++ make_bundle.sh
@@ -1,5 +1,1 @@
-mkdir ../../SnortUnified/
-rm -rf ../../SnortUnified/*
-cp -a * ../../SnortUnified/
-
-tar -X exclude.txt -cvzf dist/SnortUnified_Perl.`date +%Y%m%d`.tgz ../../SnortUnified/
+tar -X exclude.txt -cvzf dist/SnortUnified_Perl.`date +%Y%m%d`.tgz ../snort-unified-perl/*
