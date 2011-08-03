1fee2fc4575d10c2aff8505c3b289849fb6072fc
vrybdpkt@gmail.com <vrybdpkt@gmail.com@42d883e9-bd39-0410-b77b-8f19f4552143>
2011-08-03 14:21:20 +0000

    fix typo

--- trunk/SnortUnified/Database.pm
+++ trunk/SnortUnified/Database.pm
@@ -149,7 +149,7 @@ sub setSnortConnParam($$) {
             ";host=" . $DB_INFO->{'host'} .
             ";port=" . $DB_INFO->{'port'} .
             ";";
-    } else if ($DB_INFO->{'type'} eq 'ODBC' ) {
+    } elsif ($DB_INFO->{'type'} eq 'ODBC' ) {
 		$DB_INFO->{'connstr'} = "DBI:ODBC:DRIVER={SQL Server}" .
 		    ";Server=" . $DB_INFO->{'host'} . 
 			";Database=" . $DB_INFO->{'database'} .
